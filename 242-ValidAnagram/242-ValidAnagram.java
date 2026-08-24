cc9e4c7831c8e33fc3a72d59f10f3bff805ee3b5
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-08-24 21:42:14 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,30 @@
+// Last updated: 8/24/2026, 9:42:14 PM
+class Solution {
+    public boolean isAnagram(String a, String b) {
+
+        if(a.length() != b.length()){
+            return false;
+        }
+        
+        char arr[] = a.toCharArray();
+        char brr[] = b.toCharArray();
+
+
+        Arrays.sort(arr);
+        Arrays.sort(brr);
+
+
+        int l = 0;
+        int r = arr.length - 1;
+
+        while (l <= r) {
+            if (arr[l] == brr[l] && arr[r] == brr[r]) {
+                l++;
+                r--;
+            } else {
+               return false;
+            }
+        }
+    return true;
+    }
+}
