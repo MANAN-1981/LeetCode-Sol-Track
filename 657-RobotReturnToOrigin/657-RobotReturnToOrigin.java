7844cde5ffb3fb3318ac507344f16c314c0cc79e
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-08-24 21:41:39 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.java
@@ -0,0 +1,26 @@
+// Last updated: 8/24/2026, 9:41:38 PM
+class Solution {
+    public boolean judgeCircle(String moves) {
+        
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < moves.length(); i++) {
+           if(moves.charAt(i) == 'R'){
+               x++;
+           }else if (moves.charAt(i) == 'L') {
+               x--;
+           }else if (moves.charAt(i) == 'U'){
+               y++;
+           }else if (moves.charAt(i) == 'D'){
+               y--;
+           } 
+        }
+        
+        if(x == 0 && y == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
