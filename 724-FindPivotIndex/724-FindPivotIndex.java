3d16562f81bc35d78bf5593b0185402d53781c4b
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-08-24 21:41:32 +0530

    Create 724-FindPivotIndex

---
+++ 724-FindPivotIndex/724-FindPivotIndex.java
@@ -0,0 +1,81 @@
+// Last updated: 8/24/2026, 9:41:31 PM
+// class Solution {
+//     public int pivotIndex(int[] arr) {
+
+//         int n = arr.length;
+
+//         for (int i = 0; i < n; i++) {
+//             int left = leftSum(arr, i - 1);
+//             int right = rightSum(arr, i + 1, n);
+
+//             if (left == right) {
+//                 return i;
+//             } 
+//         }
+//         return -1;
+//     }
+
+//     public static int leftSum(int arr[], int index) {
+//         int sum = 0;
+
+//         if (index >= 0) {
+//             sum = arr[index] + leftSum(arr, index - 1);
+//         }
+//         return sum;
+//     }
+
+//     public static int rightSum(int arr[], int index, int n) {
+//         int sum = 0;
+
+//         if (index <= n - 1) {
+//             sum = arr[index] + rightSum(arr, index + 1, n);
+//         }
+//         return sum;
+//     }
+
+
+// static {
+//     Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+//         try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
+//             fw.write("0");
+//         } catch (Exception e) {
+//         }
+//     }));
+//     }
+// }
+
+
+class Solution {
+    public int pivotIndex(int[] arr) {
+
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            int left = leftSum(arr, i - 1);
+            int right = rightSum(arr, i + 1, n);
+
+            if (left == right) {
+                return i;
+            } 
+        }
+        return -1;
+    }
+
+    public static int leftSum(int arr[], int index) {
+        int sum = 0;
+
+        if (index >= 0) {
+            sum = arr[index] + leftSum(arr, index - 1);
+        }
+        return sum;
+    }
+
+    public static int rightSum(int arr[], int index, int n) {
+        int sum = 0;
+
+        if (index <= n - 1) {
+            sum = arr[index] + rightSum(arr, index + 1, n);
+        }
+        return sum;
+    }
+}
