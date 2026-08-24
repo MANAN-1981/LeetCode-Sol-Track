07195f2d164e67f6e4df08520dc58cea1e57dada
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-08-24 21:40:10 +0530

    Create 1797-GoalParserInterpretation

---
+++ 1797-GoalParserInterpretation/1797-GoalParserInterpretation.java
@@ -0,0 +1,21 @@
+// Last updated: 8/24/2026, 9:40:10 PM
+class Solution {
+    public String interpret(String command) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<command.length(); i++){
+           if(command.charAt(i) == '(' && command.charAt(i+1) == ')'){
+            sb.append("o");
+            i++;
+           }else if(command.charAt(i) == '(' && command.charAt(i+1) == 'a' && command.charAt(i+2) == 'l' && command.charAt(i+3) == ')'){
+            sb.append("al");
+            i = i + 3;
+           }else{
+            sb.append(command.charAt(i));
+           }
+        }
+
+        return sb.toString();
+
+    }
+}
