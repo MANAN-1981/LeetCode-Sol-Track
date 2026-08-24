6d3f27a8265e73b93fb898c47d679edd2b438d03
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-08-24 21:41:43 +0530

    Create 643-MaximumAverageSubarrayI

---
+++ 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.java
@@ -0,0 +1,37 @@
+// Last updated: 8/24/2026, 9:41:43 PM
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int left = 0;
+		int right = (int)k-1;
+		
+		int n = nums.length;
+		
+		double windowAvg = 0;
+		double maxAvg = Double.NEGATIVE_INFINITY;
+		
+		double sum = 0;
+		    for(int i=left; i<=right; i++){
+		        sum += nums[i];
+		    }
+		    windowAvg  = sum/k;
+		    
+		    if(maxAvg<windowAvg){
+		        maxAvg = windowAvg;
+		    }
+		right++;
+
+		while(right<n){
+            sum = sum - nums[left] + nums[right];
+
+            windowAvg = sum/k;
+
+		    if(maxAvg<windowAvg){
+		        maxAvg = windowAvg;
+		    }
+            
+		    left++;
+		    right++;
+		}
+    return maxAvg;
+    }
+}
