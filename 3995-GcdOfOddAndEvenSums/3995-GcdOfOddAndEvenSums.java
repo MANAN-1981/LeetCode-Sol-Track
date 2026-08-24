f9d5c9b31f6a34b97168d74c251d79a21c3e9a9c
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-08-24 21:39:23 +0530

    Create 3995-GcdOfOddAndEvenSums

---
+++ 3995-GcdOfOddAndEvenSums/3995-GcdOfOddAndEvenSums.java
@@ -0,0 +1,47 @@
+// Last updated: 8/24/2026, 9:39:22 PM
+class Solution {
+
+     public static int sumOdd(int a){
+        int sum = 0;
+        int count = 0;
+        int num = 1;
+
+        while (count < a) {
+            sum += num;
+            num += 2;
+            count++;
+        }
+        return sum;
+    }
+    
+    public static int sumEven(int a){
+        int sum = 0;
+        int count = 0;
+        int num = 2;
+
+        while (count < a) {
+            sum += num;
+            num += 2;
+            count++;
+        }
+        return sum;
+    }
+   
+    public static int GCD(int a , int b){
+        
+         if (b == 0) {
+            return a;
+        }
+        return GCD(b, a % b);
+    }
+
+
+    public int gcdOfOddEvenSums(int n) {
+        
+        int odd = sumOdd(n);
+	    int even = sumEven(n);
+	    int answer = GCD(odd,even);
+
+        return answer;
+    }
+}
