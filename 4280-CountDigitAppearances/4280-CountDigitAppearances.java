39c39bc73c32215931819670bdfa73448affb1ea
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-08-24 22:08:48 +0530

    Create 4280-CountDigitAppearances

---
+++ 4280-CountDigitAppearances/4280-CountDigitAppearances.java
@@ -0,0 +1,24 @@
+// Last updated: 8/24/2026, 10:08:48 PM
+class Solution {
+    public int countDigitOccurrences(int[] nums, int digit) {
+
+        int count = 0;
+
+        for (int k = 0; k < nums.length; k++) {
+
+            while (nums[k] != 0) {
+
+                int val = nums[k] % 10;
+
+                if (val == digit) {
+                    count++;
+                }
+
+                nums[k] = nums[k] / 10;
+            }
+        }
+
+        return count;
+
+    }
+}
