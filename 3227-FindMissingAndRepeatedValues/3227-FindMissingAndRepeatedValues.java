a09546ce2ac5525dfe65353dc424ae7eb3ce116e
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-08-24 21:39:32 +0530

    Create 3227-FindMissingAndRepeatedValues

---
+++ 3227-FindMissingAndRepeatedValues/3227-FindMissingAndRepeatedValues.java
@@ -0,0 +1,35 @@
+// Last updated: 8/24/2026, 9:39:31 PM
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+
+        HashSet<Integer> hs = new HashSet<>();
+        int result[] = new int[2];
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (!hs.add(grid[i][j])) {
+                    result[0] = grid[i][j];
+                }
+            }
+        }
+
+        int sum1 = 0;
+
+        for (int i = 1; i <= rows * cols; i++) {
+            sum1 += i;
+        }
+
+        int sum2 = 0;
+
+        for (int element : hs) {
+            sum2 += element;
+        }
+
+        result[1] = sum1 - sum2;
+
+        return result;
+    }
+}
