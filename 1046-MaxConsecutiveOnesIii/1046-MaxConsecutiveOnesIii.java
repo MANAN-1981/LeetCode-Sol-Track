955b461cabdc195d394f40683a859947665c3ed8
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-08-24 21:41:06 +0530

    Create 1046-MaxConsecutiveOnesIii

---
+++ 1046-MaxConsecutiveOnesIii/1046-MaxConsecutiveOnesIii.java
@@ -0,0 +1,36 @@
+// Last updated: 8/24/2026, 9:41:05 PM
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        
+        int n = nums.length;
+	    
+	    int l = 0;
+	    
+	    int maxWindow = 0;
+	    int count = 0;
+	    
+	    
+	    for(int i=0; i<n; i++){
+	        int cWindow = 0;
+	        
+	        if(nums[i] == 0){
+	            count++;
+	        }
+	        
+	       while(count>k){
+	           if(nums[l] == 0){
+	               count--;
+	           }
+	           l++;
+	       }
+	       
+	       cWindow = i-l+1;
+	       
+	       if(cWindow>maxWindow){
+	           maxWindow = cWindow;
+	       }
+	    }
+
+        return maxWindow;
+    }
+}
