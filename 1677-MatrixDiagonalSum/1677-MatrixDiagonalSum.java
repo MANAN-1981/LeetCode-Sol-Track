1e6fff86031b7fa85d5251fb7292bc67deee6cc0
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-08-24 21:40:21 +0530

    Create 1677-MatrixDiagonalSum

---
+++ 1677-MatrixDiagonalSum/1677-MatrixDiagonalSum.java
@@ -0,0 +1,24 @@
+// Last updated: 8/24/2026, 9:40:20 PM
+class Solution {
+    public int diagonalSum(int[][] mat) {
+
+        int n = mat.length;
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i == j) {
+                    sum += mat[i][j];
+                }
+
+                else if (i + j == n - 1) {
+                    sum += mat[i][j];
+                }
+            }
+        }
+
+        return sum;
+    }
+}
