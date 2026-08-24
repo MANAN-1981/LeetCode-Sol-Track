c4d23267e349a62a347146e26cd7535252c1d0bd
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-08-24 21:39:17 +0530

    Create 3918-CheckDivisibilityByDigitSumAndProduct

---
+++ 3918-CheckDivisibilityByDigitSumAndProduct/3918-CheckDivisibilityByDigitSumAndProduct.java
@@ -0,0 +1,23 @@
+// Last updated: 8/24/2026, 9:39:16 PM
+class Solution {
+    public boolean checkDivisibility(int n) {
+        
+        int num = n;
+        int mulNum = n;
+        int sum = 0;
+        int mul = 1;
+
+         while(num!=0){
+            sum += num%10;  
+            num = num/10;
+            
+            mul *= mulNum%10;  
+            mulNum = mulNum/10;
+        }
+
+        if(n % (sum+mul) == 0){
+            return true;
+        }
+        return false;
+    }
+}
