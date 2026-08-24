c6be32a635bcb49c7c1149dbdf9c55ecfc45c3b4
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-08-24 22:08:59 +0530

    Create 2497-MaximumMatchingOfPlayersWithTrainers

---
+++ 2497-MaximumMatchingOfPlayersWithTrainers/2497-MaximumMatchingOfPlayersWithTrainers.java
@@ -0,0 +1,25 @@
+// Last updated: 8/24/2026, 10:08:59 PM
+class Solution {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        
+         Arrays.sort(players);
+        Arrays.sort(trainers);
+
+        int i = 0;
+        int j = 0;
+        int count = 0;
+
+        while (i < players.length && j < trainers.length) {
+
+            if (players[i] <= trainers[j]) {
+                count++;
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+
+        return count;
+    }
+}
