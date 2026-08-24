1422e2f4d63a3d069e8b2ad248839422d587723a
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-08-24 21:39:22 +0530

    Create 4058-ComputeAlternatingSum

---
+++ 4058-ComputeAlternatingSum/4058-ComputeAlternatingSum.java
@@ -0,0 +1,35 @@
+// Last updated: 8/24/2026, 9:39:21 PM
+class Solution {
+    public int alternatingSum(int[] nums) {
+
+        int n = nums.length;
+
+        int l = 0;
+        int r = n - 1;
+
+        int sum = 0;
+
+        if (n == 1) {
+            return nums[0];
+        }
+        while (l <= r) {
+            if (l % 2 == 0) {
+                sum += nums[l];
+            } else if (l % 2 != 0) {
+                sum -= nums[l];
+            }
+
+            if (l != r) {
+                if (r % 2 == 0) {
+                    sum += nums[r];
+                } else {
+                    sum -= nums[r];
+                }
+            }
+
+            l++;
+            r--;
+        }
+        return sum;
+    }
+}
