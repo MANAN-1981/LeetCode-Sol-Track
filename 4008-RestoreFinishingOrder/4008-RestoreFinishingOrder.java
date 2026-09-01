394d00e24531a58142f33a7abee014db9dbd8fa0
MANAN ABASANA <131481101+MANAN-1981@users.noreply.github.com>
2026-09-01 11:14:00 +0530

    Create 4008-RestoreFinishingOrder

---
+++ 4008-RestoreFinishingOrder/4008-RestoreFinishingOrder.java
@@ -0,0 +1,20 @@
+// Last updated: 9/1/2026, 11:14:00 AM
+class Solution {
+    public int[] recoverOrder(int[] order, int[] friends) {
+        
+
+        int[] ans = new int[friends.length];
+        int index = 0;
+
+        for (int i : order) {
+            for (int j : friends) {
+                if (i == j) {
+                    ans[index++] = i;
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
